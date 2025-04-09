56e3f73f9768c585d31f437d3427f4e3ea820242
Willian Dos Santos <114609826+wdosantos6@users.noreply.github.com>
2025-04-09 20:21:02 -0300

    Add files via upload

--- ex2.c
+++ ex2.c
@@ -5,31 +5,24 @@ int ehMultiplo(int num1, int num2){
     int geraNum(int d1, int d2, int d3){
         if (ehMultiplo(d1,d2)&&ehMultiplo(d2,d3))
         {
-            return d1+d2+d3;
+            return abs(d1+d2+d3);
         }
         else if(ehMultiplo(d2,d1) && ehMultiplo(d3,d2)){
-            return d1*d2*d3;
+            return abs(d1*d2*d3);
         }
         else{
-            return (d1+d2)*(d2-d3);
+            return abs((d1+d2)*(d2-d3));
         }
 
     }
-    void verificaNum(int dia1,int dia2, int dia3){
-        if (dia1>=31|| dia2>=31 || dia3>=31)
-        { 
-            printf("inválido\n");
-        }
-        return; 
-    }
-
+    
 
 
 int main(){
     int dia1,dia2,dia3;
     printf("insira o DIA do seu aniversário,da sua mãe e do seu pai\n");
     scanf("%d %d %d", &dia1,&dia2,&dia3);
-    verificaNum(dia1,dia2,dia3);
+    
     int qualquer=geraNum(dia1,dia2,dia3);
     printf("\n%d", qualquer);
 
